cb84927f2edd4223b08530a1089f1807751b2c00
Filip Jovanovic <filip.jovanovic14@gmail.com>
2021-03-20 21:43:46 +0100

    Update main() with new function name
    Extract main into a separate function

--- python-console-scraper/main.py
+++ python-console-scraper/main.py
@@ -1,34 +1,32 @@
 import os
 import sys
 
-import requests
-
-
 from executionhelper import execute_concurrently
-from scraper.scarper import scrape_url
+from scraper.scarper import scrape_page
 
-if __name__ == "__main__":
+
+def main():
     try:
         try:
             argument = sys.argv[1]
             if not argument or argument and not argument.strip():
                 raise IndexError()
         except IndexError:
-            print("No valid parameter was passed.")
+            print("No valid parameter was passed")
             sys.exit()
 
         if os.path.isfile(argument) and os.stat(argument).st_size != 0:
             file = open(argument, "r")
             filecontent = file.read().split("\n")
             file.close()
-            execute_concurrently(scrape_url, filecontent)
+            execute_concurrently(scrape_page, filecontent)
         else:
-            try:
-                response = requests.get(argument)
-                if response and response.ok:
-                    scrape_url(response.text)
-            except requests.RequestException:
-                print("The supplied argument is not a valid URL")
+            print(scrape_page(argument))
 
-    except Exception:
+    except Exception as e:
         print("An error has occurred")
+        print(f"Exception message: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
